70fb78a01b53e9118d49b8f3675382e2ac66a283
nerw005 <peach1000211@naver.com>
2026-06-20 19:41:01 +0900

    Delete skill_analyzer directory

--- skill_analyzer/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "MainWindow.h"
-
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv); // Qt 앱 초기화
-    MainWindow   window;
-    window.show();
-    return app.exec(); // 이벤트 루프 시작
-}
